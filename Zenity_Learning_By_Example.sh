4ab8f1347b52a1c9f02ba3d33a76ebe5ed9fdb99
jonasroth@mailbase.info <jonasroth@mailbase.info>
2020-08-01 22:00:54 +0200

    Zenity L B E - Added height and height Sizing

--- Zenity_Learning_By_Example.sh
+++ Zenity_Learning_By_Example.sh
@@ -6,15 +6,19 @@ sudo apt-get install zenity
 # Error Messages not shown
 exec 2>/dev/null # Uncomment for debugging
 
+# Window Size
+h=100 # height
+w=300 # height
+
 # Question Box
-if $(`zenity  --question --title "Question Box"   --text  "Start the tour?"`); then
-	zenity --info --text "You selected <big>Ok</big>";
+if $(`zenity --height $h --width 200 --question --title "Question Box" --text "Start the tour?"`); then
+	zenity --height $h --width $w --info --text "You selected <big>Ok</big>"; # Open Window showing result
 else
 	exit;
 fi
 
 # Entry Text Selector  - No Input End Program
-if ! name=`zenity --title  "Your Name" --entry --text "Whats your name?"`; then
+if ! name=`zenity --height $h --width $w --title  "Your Name" --entry --text "Whats your name?"`; then
 	name="Ash Ketchum"; # User Selected Cancle
 fi
 
@@ -23,32 +27,31 @@ if [ -z $name ]; then
 name="Nobody"; # User klicked Ok without input
 fi
 
-# List Directory Selector - text
-if ! file1=`ls |  zenity  --list  --title "Hallo $name: ls |  zenity  --list" --text "Contents of directory" --column "Files"`; then
+# List Directory Selector in text mode
+if ! file1=`ls |  zenity  --height 400 --width $w --list  --title "Hallo $name: ls |  zenity  --list" --text "Contents of directory" --column "Files"`; then
 exit;
 fi
 
-
-# Using Variable of Name and open file-selection
-if ! file2=`zenity --title="Hallo $name, select a file!" --file-selection`; then
+# Using Variable open file-selection
+if ! file2=`zenity --height 400 --width $w --title="Hallo $name, select a file!" --file-selection`; then
 exit;
 fi
 
 # Checklist with column Definitions
-if ! choice=`zenity  --list --title "$name, does this look right?" --checklist --text "" --column "Install" --column "File" TRUE $file1 TRUE $file2`;  then
+if ! choice=`zenity --height 300 --width 400 --list --title "$name, does this look right?" --checklist --text "" --column "Install" --column "File" TRUE $file1 TRUE $file2`;  then
 exit;
 fi
 
 # Present result of Selection 
-if ! zenity --info --text "You selected: $choice"; then
+if ! zenity --height $h --width $w --info --text "You selected: $choice"; then
 exit;
 fi
 
 # Password Input
-if ! PASS=$(zenity --entry --hide-text --text "Don't enter your password." --title "PW"); then
+if ! PASS=$(zenity --height $h --width $w --entry --hide-text --text "Don't enter your password." --title "PW"); then
 exit;
 fi
 
 # Show Password
-zenity --info --text "$name\n your password is: $PASS" --title "O-o";
+zenity --height $h --width $w --info --text "$name\n your password is: $PASS" --title "O-o";
 exit;
