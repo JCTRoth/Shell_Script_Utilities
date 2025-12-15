f55712be7e9c1c67d1cb28c59f1957e34c91cde1
jonasroth@mailbase.info <jonasroth@mailbase.info>
2025-12-15 21:58:50 +0100

    Reworked Backup_Source_and_Key

--- Backup_Tools/Backup_and_Restore_Sources/Backup_Source_and_Key.sh
+++ Backup_Tools/Backup_and_Restore_Sources/Backup_Source_and_Key.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 
-# Date
-DATE=$(date +"%Y-%m-%d_%H":"%M")
-
+# Backup APT sources and trusted keys
+# Date format for backup name
+DATE=$(date +"%Y-%m-%d_%H-%M")
 
 function doBackup() {
+    local backup_name="$1"
+    local backup_dir="$backup_name"
 
-mkdir $inputValue;
+    # Create backup directory
+    if ! mkdir -p "$backup_dir"; then
+        echo "Error: Failed to create directory $backup_dir" >&2
+        return 1
+    fi
 
-find /etc/apt/sources.list* -type f -name '*.list' -exec bash -c 'echo -e "\n## $inputValue ";grep "^[[:space:]]*[^#[:space:]]" ${inputValue}' _ {} \; > ./$1/$1.list;
+    echo "Creating backup in $backup_dir..."
 
-sudo cp /etc/apt/trusted.gpg ./$1/trusted-keys.gpg; 
+    # Backup sources.list files
+    # Find all .list files in /etc/apt/sources.list.d/ and /etc/apt/sources.list
+    find /etc/apt/sources.list* -type f -name '*.list' -exec sh -c '
+        echo -e "\n## $1"
+        grep "^[[:space:]]*[^#[:space:]]" "$1"
+    ' -- {} \; > "$backup_dir/$backup_name.list"
 
-echo "Backup file written as $inputValue"
+    # Backup trusted keys
+    if sudo cp /etc/apt/trusted.gpg "$backup_dir/trusted-keys.gpg"; then
+        echo "Backup completed: $backup_name"
+    else
+        echo "Error: Failed to copy trusted keys" >&2
+        return 1
+    fi
 }
 
-#Das eingabe menü ist hier:
-echo "Do you want to use the current date as backup name?"
-read input
-if [[ $input == "Y" || $input == "y" || $input == "yes" || $input == "Yes" ]]; 
-	then
-        doBackup $DATE
+# Input menu
+echo "Do you want to use the current date as backup name? (Y/n)"
+read -r input
+if [[ $input =~ ^[Yy]$|^$ ]]; then
+    doBackup "$DATE"
 else
-        read -p "Enter the name of the backup file:  " input
-		doBackup $input
+    read -p "Enter the name of the backup file: " input
+    doBackup "$input"
 fi
 
 
